47bdf7c5636fd04c14dc79965994872571f1cba4
adevmir <ademirvalonqar@gmail.com>
2022-02-08 20:07:21 -0300

    [Requisito 1] [Correçao 08] - Arquivo certo a ser ultilizado challenges.js

--- challenges.js
+++
@@ -1,4 +0,0 @@
-// Requisito 1
-function compareTrue(value1, value2) {
-  return value1 && value2;
-}
--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,8 @@
 // Desafio 1
-function compareTrue() {
-  // seu código aqui
+function compareTrue(a, b) {
+  return a && b;
 }
+console.log(compareTrue(true, true));
 
 // Desafio 2
 function calcArea() {
