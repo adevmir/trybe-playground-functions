5c20e061a7e92b925db316607909137c8d951f4f
adevmir <ademirvalonqar@gmail.com>
2022-06-07 13:21:19 -0300

    [Requisito 11] Verifica se um array tem exatamente 11 numeros, se nenhum deles e maior que 9 ou menor que 0 e se nenhum e repetido mais de duas vezes. Se tudo passar escreve o array em forma de Numero Telefonico.

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,7 +1,50 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function phoneNumber(valid, array) {
+  let result = '';
+  if (valid > 2) {
+    result = 'não é possível gerar um número de telefone com esses valores';
+  } else {
+    array.splice(7, 0, '-');
+    array.splice(2, 0, ' ');
+    array.splice(2, 0, ')');
+    result = `(${array.join('')}`;
+  }
+  return result;
 }
+
+function quantRepeat(array, repeat) {
+  array.forEach((element) => {
+    for (let cont = 0; cont < array.length; cont += 1) {
+      if (element === array[cont]) {
+        repeat += 1;
+      }
+    } if (repeat <= 2) {
+      repeat = 0;
+    }
+  });
+  return repeat;
+}
+
+function contnumber(array) {
+  for (let a of array) {
+    if (a > 9 || a < 0) {
+      return 3;
+    }
+  }
+  return quantRepeat(array, 0);
+}
+
+function generatePhoneNumber(array) {
+  let result = '';
+  if (array.length !== 11) {
+    result = 'Array com tamanho incorreto.';
+  } else {
+    result = (phoneNumber(contnumber(array), array));
+  }
+  return result;
+}
+
+console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
 
 // Desafio 12
 function triangleCheck() {
