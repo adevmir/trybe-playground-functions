ab884c0f7349b2e8b8c0fd63acc79409fc3b34e0
adevmir <ademirvalonqar@gmail.com>
2022-07-15 16:13:49 -0300

    [Requisito Bonus 12] - Avalia se 3 medidas podem formar um triangulo

--- src/challenges2.js
+++ src/challenges2.js
@@ -47,10 +47,35 @@ function generatePhoneNumber(array) {
 console.log(generatePhoneNumber([5, 2, 8, 1, 5, 3, 7, 2, 8, 9, 0]));
 
 // Desafio 12
-function triangleCheck() {
-  // seu código aqui
+function avalDif(dif, arr, i) {
+  switch (arr[i]) {
+  case arr[0]:
+    dif = Math.abs(arr[1] - arr[2]);
+    break;
+  case arr[1]:
+    dif = Math.abs(arr[0] - arr[2]);
+    break;
+  default:
+    dif = Math.abs(arr[0] - arr[1]);
+  }
+  if (arr[i] > dif) {
+    return true;
+  }
+  return false;
 }
 
+function triangleCheck(lineA, lineB, lineC) {
+  const arr = [lineA, lineB, lineC];
+  let result = false;
+  let dif = '';
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] < (arr[0] + arr[1] + arr[2] - arr[i])) {
+      result = avalDif(dif, arr, i);
+    } else result = false;
+  }
+  return result;
+}
+triangleCheck(10, 14, 8);
 // Desafio 13
 function hydrate() {
   // seu código aqui
